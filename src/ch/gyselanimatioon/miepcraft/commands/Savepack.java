4c90be48c7d0ddb0166c57a2830bc60750890b2b
Fabian Gysel <fabiangysel15@gmail.com>
2016-07-01 14:15:02 +0200

    logger import von savepack & backpack entfernt

--- src/ch/gyselanimatioon/miepcraft/commands/Savepack.java
+++ src/ch/gyselanimatioon/miepcraft/commands/Savepack.java
@@ -7,7 +7,6 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.libs.jline.internal.Log;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
